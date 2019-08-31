c4a5034c373e9356c3e9b1c94d3750c7300542a4
aayu24 <aayupatni@gmail.com>
2019-09-01 02:30:35 +0530

    2 test cases still failing: Tasks 2 and 3 partially done

--- pintos/src/threads/thread.h
+++ pintos/src/threads/thread.h
@@ -87,14 +87,18 @@ struct thread
     enum thread_status status;          /* Thread state. */
     char name[16];                      /* Name (for debugging purposes). */
     uint8_t *stack;                     /* Saved stack pointer. */
-    int orig_priority;                  /* Original priority */
-    int64_t wakeup_at;                  /* wakeup time*/ 
     int priority;                       /* Priority. */
+    int old_priority;                   /* Old Priority. */
+    int nice;                           /* Nice Value. */
+    int recent_cpu;                     /* Approximation of recent 
+                                           cpu time used by this thread. */
+    int64_t wakeup_at;                  /* Time to wait before unblock. */
     struct list_elem allelem;           /* List element for all threads list. */
 
     /* Shared between thread.c and synch.c. */
     struct list_elem elem;              /* List element. */
 
+    struct list_elem sleepers_elem;     /* List element for sleepers_list. */
 #ifdef USERPROG
     /* Owned by userprog/process.c. */
     uint32_t *pagedir;                  /* Page directory. */
@@ -102,7 +106,9 @@ struct thread
 
     /* Owned by thread.c. */
     unsigned magic;                     /* Detects stack overflow. */
-  };
+    struct list locks_acquired;         /* List of locks acquired by a thread */
+    bool no_yield;
+};
 
 /* If false (default), use round-robin scheduler.
    If true, use multi-level feedback queue scheduler.
@@ -121,6 +127,9 @@ tid_t thread_create (const char *name, int priority, thread_func *, void *);
 void thread_block (void);
 void thread_unblock (struct thread *);
 
+void thread_block_till (int64_t);
+void thread_set_next_wakeup (void);
+
 struct thread *thread_current (void);
 tid_t thread_tid (void);
 const char *thread_name (void);
@@ -134,14 +143,21 @@ void thread_foreach (thread_action_func *, void *);
 
 int thread_get_priority (void);
 void thread_set_priority (int);
+int thread_get_effective_priority (struct thread *);
+
+void thread_update_priority (struct thread *);
+void thread_update_recent_cpu (struct thread *);
+void thread_update_load_avg (void);
 
 int thread_get_nice (void);
 void thread_set_nice (int);
 int thread_get_recent_cpu (void);
 int thread_get_load_avg (void);
-void thread_priority_temporarily_up();
-void thread_block_till(int64_t,int);
-void thread_priority_restore();
-void thread_set_next_wakeup();
+
+bool priority_cmp (const struct list_elem*, const struct list_elem*, void*);
+bool before (const struct list_elem*, const struct list_elem*, void*);
+
+void thread_priority_temporarily_up (void);
+void thread_priority_restore (void);
 
 #endif /* threads/thread.h */
